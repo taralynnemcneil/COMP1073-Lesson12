fd244e7fa3fe637f15993ea73b4fdbe336111b9f
Tara McNeil <taralynnemcneil@gmail.com>
2016-04-20 18:37:52 -0400

    app.js createJS section

--- Scripts/app.js
+++ Scripts/app.js
@@ -6,22 +6,47 @@
     "use strict";
 
     // Load the navbar dynamically via jQuery and Ajax
-    var header = $('#mainHeader').load("partials/nav.html", function(){
+    var header = $('#mainHeader').load("partials/nav.html", function() {
         var bodyid = $('body').attr('id');
         switch (bodyid.toString()) {
             case "index":
-                $('#indexLink').attr("class","active");
+                $('#indexLink').attr("class", "active");
                 break;
             case "projects":
-                $('#projectsLink').attr("class","active");
+                $('#projectsLink').attr("class", "active");
                 break;
             case "contact":
-                $('#contactLink').attr("class","active");
+                $('#contactLink').attr("class", "active");
                 break;
         }
     });
 
+    // ============== CreateJS Section ============== //
+
+    var canvas;
+    var stage;
+
+    function init() {
+        // reference to canvas element
+        canvas = document.getElementById("canvas");
+
+        // creat a stage container object
+        stage = new createjs.Stage(canvas);
+
+        // sets frame rate to 60 frames per second
+        createjs.Ticker.setFPS(60);
+       
+        // listen for frame changes and call animationLoop function
+        createjs.Ticker.addEventListener("tick", animationLoop);
+    }
     
-    
+    // runs every frame
+    function animationLoop() {
+        // refresh the stage
+        stage.update();
+    }
+
+    window.load = init;
+
 })();
 
