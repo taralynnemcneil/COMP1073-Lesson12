3d7f455820077ddfa06e383d7b8feb2007d335bd
Tara McNeil <taralynnemcneil@gmail.com>
2016-04-20 17:31:01 -0400

    Initial Commit

---
+++ Scripts/app.js
@@ -0,0 +1,25 @@
+// setup your IIFE (Immediately Invoked Function Expression)
+(function() {
+
+    "use strict";
+
+    var header = $('#mainHeader').load("partials/nav.html", function(){
+        var bodyid = $('body').attr('id');
+        switch (bodyid.toString()) {
+            case "index":
+                $('#indexLink').attr("class","active");
+                break;
+            case "projects":
+                $('#projectsLink').attr("class","active");
+                break;
+            case "contact":
+                $('#contactLink').attr("class","active");
+                break;
+        }
+        
+        
+    });
+
+    
+})();
+
